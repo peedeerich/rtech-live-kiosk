e27b37a35d1b2e402fcf25a9a497d512e3dd77a2
Paul Richards <paul@topscreenmedia.com>
2017-09-21 17:39:14 +0100

    add links on main screen

--- src/App.js
+++ src/App.js
@@ -7,12 +7,31 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          Topscreen Media
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container"> 
+          <a href="https://www.youtube.com/embed/XPVFD5vzXIU?autoplay=1&rel=0" className="tile">
+            <div>
+              Show Reel
+            </div>
+          </a>
+          <a href="http://topscreenkiosk.co.uk/towers-cms/" className="tile">
+            <div>
+              Booking Pod
+            </div>
+          </a>
+          
+          <a href="https://s3-eu-west-1.amazonaws.com/emmasdiary-photosbucket-1e6ddqba7em08/Network+Digital+Menu+Board+Range_New.pdf" className="tile">
+            <div>
+              Menu Board
+            </div>
+          </a>
+          <a href="http://topscreenmedia.com/products/kiosks/" className="tile">
+            <div>
+              Website
+            </div>
+          </a>
+        </div>
       </div>
     );
   }
